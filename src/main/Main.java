60f73d8a6e477c0264cee0ea1fc2bd5cb7f2d785
NellyHargeliya <nelly.hargeliy@gmail.com>
2016-07-21 23:51:18 +0300

    add test

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,5 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        Problem3 pr = new Problem3();
-        for (long arr : pr.maxProduct(6)) {
-            System.out.print(arr + " ");
-        }
     }
 }

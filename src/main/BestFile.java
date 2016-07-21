6ae9ef36fdf4c1d8b3993e70160a78b99117e314
NellyHargeliya <nelly.hargeliy@gmail.com>
2016-07-21 23:34:19 +0300

    add describe functions

--- src/main/BestFile.java
+++ src/main/BestFile.java
@@ -1,12 +1,26 @@
 package main;
 
 /**
- * Created by Nelly on 16.06.2016.
+ * Created by Nelly
  */
 public interface BestFile {
+    /**
+     * Returns a string  with  added arithmetic operations.
+     * The operations of addition and subtraction.
+     * If this is not possible, return an empty string.
+     */
     String fitPlusMinus(String digits, long expectedResult);
 
+    /**
+     * Same as in the previous paragraph, but to the list of allowed operations
+     * added multiplication and division.
+     */
     String fit(String digits, long expectedResult);
+
+    /**
+     * Same as in the previous paragraph, in addition to arithmetic operations
+     * can be add brackets.
+     */
 
     String fitBraces(String digits, long expectedResult);
 
